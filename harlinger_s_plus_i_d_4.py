ae8513a722102ba9d8ce38c280f35f7f94364297
daimonie <daimonie@Atlantis>
2015-09-29 11:55:57 +0200

    Equation 4 is done. Onwards, to d+i.d

--- harlinger_s_plus_i_d_4.py
+++ harlinger_s_plus_i_d_4.py
@@ -17,19 +17,23 @@ k_x, k_y = np.meshgrid(
    np.linspace(-numberOfCells, numberOfCells, resolution)
 );
 
-gamma = 0.3;
-relativeDelta = (1+gamma**2)*(np.cos(k_x*2*np.pi) - np.cos(k_y*2*np.pi)) - gamma**2;
+epsilon = 0.3;
+
+oscillation = np.cos(2*np.pi*k_x) - np.cos(2*np.pi*k_y);
+
+relativeDeltaReal = epsilon*oscillation;
+relativeDeltaImag = (1-epsilon)*oscillation;
 
 fig = plt.figure();
-ax = fig.add_subplot(111,projection='3d');
+ax = fig.add_subplot(121,projection='3d');
 
 offset = numberOfCells+2;
 limit  = 20;
 
-ax.plot_surface(k_x, k_y, relativeDelta, rstride=8, cstride=8, alpha=0.3)
-cset = ax.contour(k_x, k_y, relativeDelta, zdir='z', offset=-offset, cmap=cm.coolwarm)
-cset = ax.contour(k_x, k_y, relativeDelta, zdir='x', offset=-offset, cmap=cm.coolwarm)
-cset = ax.contour(k_x, k_y, relativeDelta, zdir='y', offset=offset, cmap=cm.coolwarm)
+ax.plot_surface(k_x, k_y, relativeDeltaReal, rstride=8, cstride=8, alpha=0.3)
+cset = ax.contour(k_x, k_y, relativeDeltaReal, zdir='z', offset=-offset, cmap=cm.coolwarm)
+cset = ax.contour(k_x, k_y, relativeDeltaReal, zdir='x', offset=-offset, cmap=cm.coolwarm)
+cset = ax.contour(k_x, k_y, relativeDeltaReal, zdir='y', offset=offset, cmap=cm.coolwarm)
  
 ax.set_xlabel('k_x')
 ax.set_xlim(-(numberOfCells+1) , (numberOfCells+1))
@@ -38,4 +42,22 @@ ax.set_ylim(-(numberOfCells+1) , (numberOfCells+1) )
 ax.set_zlabel('Delta/Delta_0')
 ax.set_zlim(-offset , limit)
 
+plt.title("Real");
+
+
+ax2 = fig.add_subplot(122,projection='3d');
+ 
+ax2.plot_surface(k_x, k_y, relativeDeltaImag, rstride=8, cstride=8, alpha=0.3)
+cset = ax2.contour(k_x, k_y, relativeDeltaImag, zdir='z', offset=-offset, cmap=cm.coolwarm)
+cset = ax2.contour(k_x, k_y, relativeDeltaImag, zdir='x', offset=-offset, cmap=cm.coolwarm)
+cset = ax2.contour(k_x, k_y, relativeDeltaImag, zdir='y', offset=offset, cmap=cm.coolwarm)
+ 
+ax2.set_xlabel('k_x')
+ax2.set_xlim(-(numberOfCells+1) , (numberOfCells+1))
+ax2.set_ylabel('k_y')
+ax2.set_ylim(-(numberOfCells+1) , (numberOfCells+1) )
+ax2.set_zlabel('Delta/Delta_0')
+ax2.set_zlim(-offset , limit)
+
+plt.title("Imaginary");
 plt.show()
