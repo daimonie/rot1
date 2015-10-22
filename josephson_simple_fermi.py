a7550ad926feaba08978719efbba5b7427b83814
daimonie <daimonie@Atlantis.Atlantis>
2015-10-22 20:59:08 +0200

    update

--- josephson_simple_fermi.py
+++ josephson_simple_fermi.py
@@ -1,12 +1,23 @@
 import numpy as np;
 import scipy as sp;
 from scipy.constants import *
+#params are nice...
+import argparse as argparse
 #plotting
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 #script
+#arguments
+parser = argparse.ArgumentParser(prog="python main.py",
+  description = "Takes Delta(k) and plots phase/amp graphs. Use -help to see more information."); 
+parser.add_argument('-n', '--number', help='Resolution onf the calculation.', default = 10, action='store', type = int);
+args = parser.parse_args();
+
+
+N = args.number;
+#some constants
 hbar =  physical_constants["Planck constant over 2 pi"][0]
 mass =  physical_constants["electron mass"][0]
 ev =  physical_constants["electron volt"][0]
@@ -20,10 +31,9 @@ kFermi = 3*np.pi/4;
 #linspace is really slow. I'm serious. Let's not use it :-)
 #arange is about seven times faster. See test.py
 
-#Due to time constraints, we're going for dirty for loops. Sorry.
-N = 10
+#Due to time constraints, we're going for dirty for loops. Sorry. 
 for flux in np.arange(-5, 5, 10.0/N):
-	for delta in np.arange(2*eta/N, 2*eta, 2*eta/N):
+	for delta in np.arange(2*deltas/N, 2*deltas, 2*deltas/N):
 		current = 0;
 		for phi in np.arange(0,N, 2 * np.pi / N): 
 			for k in np.arange(0, kFermi, kFermi/N):
@@ -40,10 +50,7 @@ for flux in np.arange(-5, 5, 10.0/N):
 				
 				current += deltas*norm/(energyL+energyR)/energyL/energyR*np.sin(arg+flux*2*np.pi)
 		print "%2.3e\t%2.3e\t%2.3e" % (flux,delta,current)
-			
-			
-			
-			
+	print "" 	
 			
 			
 			
