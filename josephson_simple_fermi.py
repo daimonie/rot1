f5db946b71ceb056db3979ea15a70458babbd51a
daimonie <daimonie@Atlantis.Atlantis>
2015-10-23 09:56:28 +0200

    Hey look, results. A lot quicker too, if you don't plot things on the screen.

--- josephson_simple_fermi.py
+++ josephson_simple_fermi.py
@@ -19,12 +19,14 @@ parser.add_argument('-s', '--switch', help='Setting this to false shows dI(k,phi
 parser.add_argument('-t', '--tunnel', help='Sets the tunneling model.', default = 0, action='store', type = int);
 parser.add_argument('-a', '--parama', help='Sets a parameter for some function (debug).', default = 1.0, action='store', type = float);
 parser.add_argument('-b', '--paramb', help='Sets a parameter for some function (debug).', default = 1.0, action='store', type = float);
+parser.add_argument('-f', '--filename', help='Sets the filename. Only saves when given.', default = "default.png", action='store', type = str);
 args	= parser.parse_args();
 
 
 N	= args.number;
 switch	= args.switch;
 tunnel	= args.tunnel;
+filename= args.filename;
 parama	= args.parama;
 paramb	= args.paramb;
 #some constants
@@ -40,7 +42,8 @@ kFermi	= 3*np.pi/4
 fluxnum = 2.0;
 #linspace is really slow. I'm serious. Let's not use it :-)
 #arange is about seven times faster. See test.py
-
+if N > 100:
+	raise Exception("You know this is going to crash...");
 flux	= np.arange(-fluxnum*2*np.pi,fluxnum*2*np.pi, 2.0*fluxnum*np.pi/N)
 delta	= np.arange(2*deltaS/N, 10*deltaS, 2*deltaS/N)
 k	= np.arange(kFermi/N,kFermi, kFermi/N)
@@ -48,14 +51,19 @@ phi	= np.arange(0,2*np.pi, 2*np.pi/N)
 
 dkdphi	= kFermi * 2.0*fluxnum*np.pi/ N**2
 
-flux, delta, k, phi = np.meshgrid(flux,delta,k,phi);
-
 Heaviside = lambda xx: 1 * (xx>0)
+
+
+fig = plt.figure(figsize=(15,15))
+	
+	
 if tunnel == 0:#Constant rate; no tunneling effects
 	tunnelMatrix = lambda kk, pp: 1;
 elif tunnel==1:#
 	tunnelMatrix = lambda kk, pp:  Heaviside(np.pi*0.85-pp)*Heaviside(pp - 0.35*np.pi)*Heaviside(kk-kFermi/2.0)
 if switch == 0:
+
+	flux, delta, k, phi = np.meshgrid(flux,delta,k,phi);
 	# lambda functions
 	Delta		= lambda dd, kk, pp: dd * ( np.cos(kk*np.cos(pp)) - np.cos(kk*np.sin(pp)))
 	Energy		= lambda kk, dd: (eta*kk**2 + dd**2)**0.5
@@ -67,8 +75,7 @@ if switch == 0:
 
 	from mpl_toolkits.mplot3d import Axes3D
 	import matplotlib.pyplot as plt
-
-	fig = plt.figure()
+ 
 	ax = fig.add_subplot(111, projection='3d') 
 	ax.plot_surface(flux[..., 0, 0], delta[..., 0, 0], current, rstride=int(parama), cstride=int(paramb), cmap=cm.summer,linewidth=0, antialiased=False)
 	
@@ -77,33 +84,35 @@ if switch == 0:
 	ax.view_init(10, 80)
 	plt.xlabel("Flux")
 	plt.ylabel("Delta_0")
-	plt.title("CPR for JJ s-wave into d-wave x^2-y^2, tunnel mode %d" % tunnel)
-	plt.show()
+	plt.title("CPR for JJ s-wave into d-wave x^2-y^2, tunnel mode %d" % tunnel) 
 elif switch==1:
+	k, phi = np.meshgrid(k, phi)
+	
 	Delta		= lambda dd, kk, pp: dd * ( np.cos(kk*np.cos(pp)) - np.cos(kk*np.sin(pp)))
 	Energy		= lambda kk, dd: (eta*kk**2 + dd**2)**0.5
 	EnergyR		= lambda kk, dd, pp: (eta*kk**2 + Delta(kk,dd,pp)**2)**0.5
 	currentKPhi	= lambda ff, dd, kk, pp: tunnelMatrix(kk,pp)*dkdphi*np.abs(Delta(dd, kk, pp))*deltaS*np.sin(ff) /( (Energy(kk,dd)+EnergyR(kk,dd,pp)) * (Energy(kk,dd)*EnergyR(kk,dd,pp)))
 	
 	print "[x2-y2] You have chosen the current in k-space, where Flux is set to pi*%2.3f and Delta-parameter is set to DeltaS*%2.3f, with tunneling model %d." % (parama,paramb, tunnel)
-	#currentKPhi plot. Is apparently needed :-)
-	k	= np.arange(kFermi/N,kFermi, kFermi/N)
-	phi	= np.arange(0,2*np.pi, 2*np.pi/N)
-	k, phi = np.meshgrid(k,phi)
+	#currentKPhi plot. Is apparently needed :-)  
 	current = currentKPhi(np.pi*parama,deltaS*paramb,k,phi);
  
 	from mpl_toolkits.mplot3d import Axes3D
 	import matplotlib.pyplot as plt
-
-	fig = plt.figure()
+ 
 	ax = fig.add_subplot(111, projection='3d') 
-	ax.plot_surface(k, phi, current)
-	plt.xlabel("magnitude k")
-	plt.ylabel("angle k")
-	plt.title("CPR term under sumfor JJ s-wave into d-wave x^2-y^2, tunnel mode %d" % tunnel)
-	plt.show()
 	
+	
+	x = k*np.cos(phi);
+	y = k*np.sin(phi);
+	
+	ax.plot_surface(x, y, current, rstride=int(parama), cstride=int(paramb), cmap=cm.summer,linewidth=0, antialiased=False) 
+	plt.xlabel("x")
+	plt.ylabel("y")
+	plt.title("CPR term under sumfor JJ s-wave into d-wave x^2-y^2, tunnel mode %d" % tunnel) 
 elif switch == 2: #Let's do d_{xy}!
+
+	flux, delta, k, phi = np.meshgrid(flux,delta,k,phi)
 	# lambda functions
 	Delta		= lambda dd, kk, pp: dd * ( np.sin(kk*np.cos(pp))*np.sin(kk*np.sin(pp)))
 	Energy		= lambda kk, dd: (eta*kk**2 + dd**2)**0.5
@@ -115,8 +124,7 @@ elif switch == 2: #Let's do d_{xy}!
 
 	from mpl_toolkits.mplot3d import Axes3D
 	import matplotlib.pyplot as plt
-
-	fig = plt.figure()
+ 
 	ax = fig.add_subplot(111, projection='3d') 
 	ax.plot_surface(flux[..., 0, 0], delta[..., 0, 0], current, rstride=int(parama), cstride=int(paramb), cmap=cm.summer,linewidth=0, antialiased=False)
 	
@@ -124,28 +132,55 @@ elif switch == 2: #Let's do d_{xy}!
 	ax.view_init(10, 80)
 	plt.xlabel("Flux")
 	plt.ylabel("Delta_0")
-	plt.title("CPR for JJ s-wave into d-wave xy, tunnel mode %d" % tunnel)
-	plt.show()
+	plt.title("CPR for JJ s-wave into d-wave xy, tunnel mode %d" % tunnel) 
 elif switch==3:
+
+	k, phi = np.meshgrid(k,phi)
 	Delta		= lambda dd, kk, pp: dd * ( np.sin(kk*np.cos(pp))*np.sin(kk*np.sin(pp)))
 	Energy		= lambda kk, dd: (eta*kk**2 + dd**2)**0.5
 	EnergyR		= lambda kk, dd, pp: (eta*kk**2 + Delta(kk,dd,pp)**2)**0.5
 	currentKPhi	= lambda ff, dd, kk, pp: tunnelMatrix(kk,pp)*dkdphi*np.abs(Delta(dd, kk, pp))*deltaS*np.sin(ff) /( (Energy(kk,dd)+EnergyR(kk,dd,pp)) * (Energy(kk,dd)*EnergyR(kk,dd,pp)))
 	
 	print "[xy] You have chosen the current in k-space, where Flux is set to pi*%2.3f and Delta-parameter is set to DeltaS*%2.3f, with tunneling mode %d." % (parama,paramb, tunnel)
-	#currentKPhi plot. Is apparently needed :-)
-	k	= np.arange(kFermi/N,kFermi, kFermi/N)
-	phi	= np.arange(0,2*np.pi, 2*np.pi/N)
-	k, phi = np.meshgrid(k,phi)
+	#currentKPhi plot. Is apparently needed :-)  
 	current = currentKPhi(np.pi*parama,deltaS*paramb,k,phi);
  
 	from mpl_toolkits.mplot3d import Axes3D
 	import matplotlib.pyplot as plt
-
-	fig = plt.figure()
+ 
 	ax = fig.add_subplot(111, projection='3d') 
-	ax.plot_surface(k, phi, current)
-	plt.xlabel("magnitude k")
-	plt.ylabel("angle k")
+	
+	x = k*np.cos(phi);
+	y = k*np.sin(phi);
+	
+	ax.plot_surface(x, y, current, rstride=int(parama), cstride=int(paramb), cmap=cm.summer,linewidth=0, antialiased=False) 
+	plt.xlabel("x")
+	plt.ylabel("y")
 	plt.title("CPR term under sumfor JJ s-wave into d-wave xy, tunnel mode %d" % tunnel)
+elif switch == 4: #Tunnel matrix
+	k, phi = np.meshgrid(k,phi)
+	tunnelfdkp = lambda kk, pp: tunnelMatrix(kk,pp) 
+	
+	tunnelFunc = tunnelfdkp(k,phi)
+	
+	print "Inspecting tunnelingMatrix %d." % tunnel 
+
+	from mpl_toolkits.mplot3d import Axes3D
+	import matplotlib.pyplot as plt
+
+	ax = fig.add_subplot(111, projection='3d')  
+	ax.view_init(50, 80)
+	
+	x = k*np.cos(phi);
+	y = k*np.sin(phi);
+	
+	ax.plot_surface(x, y, tunnelFunc, rstride=int(parama), cstride=int(paramb), cmap=cm.summer,linewidth=0, antialiased=False)
+	plt.xlabel("x")
+	plt.ylabel("y")
+	plt.title("Inspecting tunnelingMatrix %d" % tunnel) 
+	
+	
+if filename != "default.png":	
+	fig.savefig(filename)
+else:
 	plt.show()
