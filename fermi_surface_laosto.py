d119a1b57a523771c3e2ab1d733240fff30523df
daimonie <daimonie@Atlantis.Atlantis>
2015-10-27 16:15:02 +0100

    Just a commit. I haven't actually changed much, because I am stuck on determining how to use the calculated data in my lambda functions. I hope stackexchange will turn up the answer soon. (Last time it took a few hours ;-) )

--- fermi_surface_laosto.py
+++ fermi_surface_laosto.py
@@ -5,14 +5,15 @@ import tinyarray as ta
 from scipy.optimize import newton
 from scipy.linalg import eigvalsh
 import sys
-
-sys.path.append('/home/bovenzi/boltzmann')
+ 
 
 from laosto import *
 from conductivity import *
 
+res = 5;
+
 kF0 = [0.5*pi, 0.5*pi, 0.5*pi, 0.5*pi, -1, -1]
-phis = np.linspace(0., 2*pi, 200, endpoint=False)
+phis = np.linspace(0., 2*pi, res, endpoint=False)
 
 system = LAOSTO(mu=0, H=30, theta=np.pi/4, g=5, gL=1, tl1=340, tl2=340, th=12.5,
                td=12.5, dE=60, dZ=15, dSO=5)
@@ -21,7 +22,13 @@ def fermi_surface(system):
     kFs, indices = kF_angle(system, phis, kF0)  
     return kFs, indices
 
-
-
-
-    
+print "\tphi\tk_x\tk_y\tk"
+Fermi, Indices = fermi_surface(system);
+for n in range(0, Indices.shape[0]):
+	start = Indices[n,1]
+	end = Indices[n,2]
+	print "%d-th band from %d to %d.\n" % (n, start, end)
+	for i in range(start,end):
+		kx = Fermi[i, 0]
+		ky = Fermi[i, 1]
+		print "\t%2.3e\t%2.3e\t%2.3e\t%2.3e" % (phis[i-n*res], kx, ky, (kx**2+ky**2)**0.5)
--- josephson.py
+++ josephson.py
@@ -22,7 +22,10 @@ parser.add_argument('-n', '--number', help='Resolution onf the calculation.', de
 parser.add_argument('-d', '--gapfunction', help='Resolution onf the calculation.', default = 10, action='store', type = int)  
 parser.add_argument('-m', '--mechanism', help='Resolution onf the calculation.', default = 10, action='store', type = int)   
 parser.add_argument('-p', '--plot', help='Resolution onf the calculation.', default = 10, action='store', type = int)  
+parser.add_argument('-k', '--fermi', help='Fermi Surface.', default = 10, action='store', type = int)  
+parser.add_argument('-b', '--band', help='Fermi Surface.', default = 10, action='store', type = int)  
 parser.add_argument('-f', '--filename', help='Sets the filename. Only saves when given.', default = "default.png", action='store', type = str) 
+parser.add_argument('-s', '--silent', help='Do not plot, do not save', default = False, action='store', type = bool) 
 args	= parser.parse_args() 
 
 
@@ -31,6 +34,9 @@ gapfunction	= args.gapfunction 	#Gap function
 mechanism	= args.mechanism 	#Scattering
 plotMode	= args.plot 		#Plot Mode
 filename	= args.filename 	#Filename if we want to save
+fermi		= args.fermi 		#Type of Fermi surface
+band		= args.band	 	#Band number. There are typically 4 bands as far as I can see.
+silent		= args.silent	 	#Silent. Don't plot, don't save.
 
 if N > 90:
 	raise Exception("An initial test showed that, while for N<90 the program is well behaved and there is a definite linear scaling,\n N>90 leads to a sudden calculation time more than four times over N=90 which meant I cancelled it");
@@ -66,19 +72,50 @@ phiArray	= np.arange(0,2*np.pi, dPhi)
 #We define the figure here so that the different modes can assign labels/titles
 fig = plt.figure(figsize=(15,15))
 #Define Lambda functions.
+Heaviside 	= lambda xx: 1.0 * (xx>0) 
+Dirac 		= lambda xx: 1.0 * (xx==0) 
 #Gap Function.
 if gapfunction == 0: #d_{x^2-y^2}
 	Delta	= lambda dd, kk, pp: dd * ( np.cos(kk*np.cos(pp)) - np.cos(kk*np.sin(pp)))
 elif gapfunction == 1: #d_{xy}
 	Delta = lambda dd, kk, pp: dd*np.sin(kk*np.cos(pp))*np.sin(kk*np.sin(pp))
+elif gapfunction == 2: #s-wave
+	Delta = lambda dd, kk, pp: dd
 else:
 	raise Exception("Unknown gap function.") 
+#By far, the easiest way to incorporate the Fermi Surface is by just letting it tag along
+#	with the tunnel functionality.
+#	the only requirement is that the Fermi surface is at least as high as the maximum calculated length.
+#	This requires us to calculate the fermi surface and, sadly, remake the kFermi, dK and kArray variables. 
+#	It is, however, the smallest step.
+if band >= 4:
+	raise Exception("There are only four bands");
+
+if fermi == 0:
+	Fermi = lambda kk, pp: 1.0;
+elif fermi == 1:
+		
+	from laosto import *
+	from conductivity import *
+	kF0 = [0.5*pi, 0.5*pi, 0.5*pi, 0.5*pi, -1, -1]
+	system = LAOSTO(mu=0, H=30, theta=np.pi/4, g=5, gL=1, tl1=340, tl2=340, th=12.5, td=12.5, dE=60, dZ=15, dSO=5)
+	
+	
+	kFermis, indices = kF_angle(system, phiArray, kF0)
+	start = indices[band, 1]
+	end = indices[band, 2]
+	
+	fermiLevel = kFermis[start:end,:]
+	fermiSurface = ((fermiLevel**2).sum(axis=1))**0.5
+	 
+else:
+	raise Exception("Unknown fermi surface requested.")
+
 #The tunnel 'function' is required for the other functions.
-Heaviside = lambda xx: 1.0 * (xx>0) 
 if mechanism == 0: #constant rate
-	tunnel = lambda kk, pp: 1.0 
+	tunnel = lambda kk, pp: 1.0 * Fermi(kk, pp)
 elif mechanism == 1: #A slice of k-space.
-	tunnel = lambda kk, pp: Heaviside(pp)*Heaviside(pp - np.pi/2)*Heaviside(kk-kFermi/2)
+	tunnel = lambda kk, pp: Heaviside(pp)*Heaviside(pp - np.pi/2)*Heaviside(kk-kFermi/2) * Fermi(kk, pp)
 else:
 	raise Exception("Unknown tunnel function.") 
 #Energies.
@@ -90,6 +127,7 @@ dCurrent	= lambda ff, dd, kk, pp:tunnel(kk,pp)*dkdphi*np.abs(Delta(dd, kk, pp))*
 ax = fig.add_subplot(111, projection='3d')  
 ax.view_init(50, 80) 
 #Mode switching 
+title = "Unknown Mode." 
 if plotMode == 0: #Plot tunnel function in k-space
 	k, phi = np.meshgrid(kArray,phiArray)
 	
@@ -100,7 +138,7 @@ if plotMode == 0: #Plot tunnel function in k-space
 	
 	plt.xlabel("k_x")
 	plt.ylabel("k_y")
-	plt.title("Inspecting tunneling matrix, N=%d, d=%d, m=%d, p=%d" % (N,gapfunction, mechanism, plotMode))
+	title = "Tunneling Matrix";
 elif plotMode == 1: 
 	flux, delta, k, phi = np.meshgrid(fluxArray,deltaArray,kArray,phiArray);
 	z = dCurrent(flux,delta, k, phi).sum(axis=-1).sum(axis=-1) 
@@ -109,8 +147,8 @@ elif plotMode == 1:
 	x,y = np.meshgrid(fluxArray, deltaArray)
 	
 	plt.xlabel("Flux")
-	plt.ylabel("Delta")
-	plt.title("Inspecting Josephson current, N=%d, d=%d, m=%d, p=%d" % (N,gapfunction, mechanism, plotMode))
+	plt.ylabel("Delta") 
+	title = "Current";
 elif plotMode == 2:  
 	ax.view_init(0, 90) 
 	#It's just mode 1 with a different view angle
@@ -122,12 +160,29 @@ elif plotMode == 2:
 	x,y = np.meshgrid(fluxArray, deltaArray) 
 	
 	plt.xlabel("Flux")
-	plt.ylabel("Delta") 
-	plt.title("Inspecting Josephson current, N=%d, d=%d, m=%d, p=%d" % (N,gapfunction, mechanism, plotMode))
+	plt.ylabel("Delta")   
+	title = "Current_k_phi";
+elif plotMode == 3:
+	
+	ax.view_init(30, 30) 
+	k, phi = np.meshgrid(kArray,phiArray)
+	
+	x = k * np.cos(phi)
+	y = k * np.sin(phi)
+	
+	z = Delta(1, k, phi);
+	
+	plt.xlabel("k_x")
+	plt.ylabel("k_y")  
+	title = "Gap function";
 else:
 	raise Exception("Unknown plot mode.");   
+plt.title("%s, N=%d, d=%d, m=%d, p=%d, k=%d, b=%d" % (title, N,gapfunction, mechanism, plotMode, fermi, band))
 ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.summer,linewidth=0, antialiased=False) 
 
+if silent:
+	print "Silent Mode; no plotting, no saving.";
+	plt.close();
 if filename != "default.png":	
 	fig.savefig(filename)
 else:
