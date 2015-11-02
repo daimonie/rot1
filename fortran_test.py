15edbece2fb130d0b7644a866d696429c949930e
daimonie <daimonie@Atlantis>
2015-11-02 14:55:39 +0100

    Added a proper stacksize variable. OpenMP should now work. Running test...

--- fortran_test.py
+++ fortran_test.py
@@ -17,58 +17,62 @@ import matplotlib.pyplot as plt
 from laosto import *
 from conductivity import * 
 
-N		= 15
-band		= 0
-#Some physical constants.
-hbar	=  physical_constants["Planck constant over 2 pi"][0]
-mass	=  physical_constants["electron mass"][0]
-ev	=  physical_constants["electron volt"][0]
-lattice	= 3.787 * physical_constants["Angstrom star"][0] #(From Wikipedia)
+#N		= 40
+for N in range(14,100):
+	band		= 0
+	#Some physical constants.
+	hbar	=  physical_constants["Planck constant over 2 pi"][0]
+	mass	=  physical_constants["electron mass"][0]
+	ev	=  physical_constants["electron volt"][0]
+	lattice	= 3.787 * physical_constants["Angstrom star"][0] #(From Wikipedia)
 
-eta	= hbar**2 / 2 / mass / ev / lattice**2
-deltaS	= 3e-3
-fluxnum = 2.0 
-#small changes
-dFlux = 4.0*fluxnum*np.pi/N
-dDelta = 10*deltaS/N
-dPhi = 2*np.pi/N
+	eta	= hbar**2 / 2 / mass / ev / lattice**2
+	deltaS	= 3e-3
+	fluxnum = 2.0 
+	#small changes
+	dFlux = 4.0*fluxnum*np.pi/N
+	dDelta = 10*deltaS/N
+	dPhi = 2*np.pi/N
 
-#Make our arrays of parameters.
-fluxArray	= np.arange(-fluxnum*2*np.pi,	fluxnum*2*np.pi+dFlux,	dFlux)
-deltaArray	= np.arange(2*deltaS/N, 	10*deltaS+dDelta, 	dDelta)
-phiArray	= np.arange(0,			2*np.pi+dPhi, 		dPhi) 
-
-
-#Calculate fermi surface
-
-kF0 = [0.5*pi, 0.5*pi, 0.5*pi, 0.5*pi, -1, -1]
-system = LAOSTO(mu=0, H=30, theta=np.pi/4, g=5, gL=1, tl1=340, tl2=340, th=12.5, td=12.5, dE=60, dZ=15, dSO=5)
+	#Make our arrays of parameters.
+	fluxArray	= np.arange(-fluxnum*2*np.pi,	fluxnum*2*np.pi+dFlux,	dFlux)
+	deltaArray	= np.arange(2*deltaS/N, 	10*deltaS+dDelta, 	dDelta)
+	phiArray	= np.arange(0,			2*np.pi+dPhi, 		dPhi) 
 
 
-kFermis, indices = kF_angle(system, phiArray, kF0)
-start = indices[band, 1]
-end = indices[band, 2]
+	#Calculate fermi surface
 
-fermiLevel = kFermis[start:end,:]
-fermiSurface = ((fermiLevel**2).sum(axis=1))**0.5
-
-#Parameters, small changes, arrays; these are here because they depend on the fermi surface calculation, which itself depends on phiArray
-kFermi		= np.max(fermiSurface)
-dK		= kFermi/N
-kArray		= np.arange(1e-15, kFermi+2*dK, dK) 
-dkdphi		= dK*dPhi;
+	kF0 = [0.5*pi, 0.5*pi, 0.5*pi, 0.5*pi, -1, -1]
+	system = LAOSTO(mu=0, H=30, theta=np.pi/4, g=5, gL=1, tl1=340, tl2=340, th=12.5, td=12.5, dE=60, dZ=15, dSO=5)
 
 
+	kFermis, indices = kF_angle(system, phiArray, kF0)
+	start = indices[band, 1]
+	end = indices[band, 2]
 
-flux, delta, k, phi = np.meshgrid(fluxArray,deltaArray,kArray,phiArray);
+	fermiLevel = kFermis[start:end,:]
+	fermiSurface = ((fermiLevel**2).sum(axis=1))**0.5
+
+	#Parameters, small changes, arrays; these are here because they depend on the fermi surface calculation, which itself depends on phiArray
+	kFermi		= np.max(fermiSurface)
+	dK		= kFermi/N
+	kArray		= np.arange(1e-15, kFermi+2*dK, dK) 
+	dkdphi		= dK*dPhi;
 
 
 
-startTime = time.time();
- 
-#time to introduce the fortran function and get a move on
+	flux, delta, k, phi = np.meshgrid(fluxArray,deltaArray,kArray,phiArray);
 
 
 
-#end of test
-print "Time elapsed is %2.3f [s]." % (time.time() - startTime);
+	startTime = time.time();
+	
+	#time to introduce the fortran function and get a move on
+	import populate as fpopulate  
+	def fermi (kk, pp): 
+		return fpopulate.populate.fermi_integrand(first=kk.shape[0], second=kk.shape[1], third=kk.shape[2], fourth=kk.shape[3],
+			fermi_surface=fermiSurface, angle_array=phiArray, angle=pp, radius=kk);
+		
+	fermi(k,phi)
+	#end of test
+	print "[%d] Time elapsed is %2.3f [s]." % (N, time.time() - startTime);
---
+++ populate.f90
@@ -0,0 +1,55 @@
+!f90
+!f2py --fcompiler=gfortran --f90flags="-fopenmp" -lgomp -m -c populate populate.f90
+!Earlier, I wrote a `dirty' function in python that is significantly slowing things down. Because I see no other implementation
+! that works, and stackexchange hasn't given me an answer either, I've chosen to write that function in openMP fortran instead.
+
+module populate
+	contains
+	subroutine fermi_integrand( first, second, third, fourth, fermi_surface, radius, angle, angle_array, fermi)
+	!fermi has to be in the argument list because it is going out. Fortran is weird that way.
+		implicit none
+		
+		!flux, delta, k, phi = np.meshgrid(fluxArray,deltaArray,kArray,phiArray);
+		integer, intent (in):: first, second, third, fourth
+		double precision, intent (in), dimension(fourth) :: fermi_surface, angle_array
+		double precision, intent (in), dimension(first,second,third,fourth):: radius, angle
+		
+		integer, intent(out), dimension(first, second, third, fourth) :: fermi
+		
+		double precision :: present_surface
+		integer :: i, j, ii, jj, a, omp_get_max_threads
+		!put fermi on the heap, whatever that is 
+		
+		!time to get some openmp in
+		
+		call omp_set_num_threads(omp_get_max_threads())
+		!$omp parallel do  &
+		!$omp default(none) &
+		!$omp private(present_surface) &
+		!$omp firstprivate(first,second,third,fourth,angle,radius,angle_array,fermi_surface) &
+		!$omp shared(fermi)
+		do i = 1, first
+			do j = 1, second
+				do ii = 1, third
+					do jj = 1, fourth
+						!current angle is phi[i,j,ii,jj]
+						!we have to find the current fermi surface
+						present_surface = 0.
+						do a = 1, fourth    
+							if (angle(i,j,ii,jj) == angle_array(a)) then
+								present_surface = fermi_surface(a)
+							end if
+						end do
+						
+						if (radius(i,j,ii,jj) >= present_surface) then 
+							fermi(i,j,ii,jj) = 0.
+						else 
+							fermi(i,j,ii,jj) = 1.
+						end if
+					end do
+				end do
+			end do
+		end do  
+		!$omp end parallel do
+	end subroutine fermi_integrand
+end module populate
