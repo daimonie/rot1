1e5ac99bc077346c2a586bd20431cb87310bb353
daimonie <daimonie@Atlantis.Atlantis>
2015-11-03 16:23:30 +0100

    I'm not sure what went wrong and broke the plots.
    I've reset it to an older version, dirty way (using the code via github).

--- josephson.py
+++ josephson.py
@@ -22,13 +22,12 @@ parser	= argparse.ArgumentParser(prog="Josephson.Py",
 parser.add_argument('-n', '--number', help='Resolution of the calculation.', default = 10, action='store', type = int)  
 parser.add_argument('-d', '--gapfunction', help='Gap function.', default = 10, action='store', type = int)  
 parser.add_argument('-m', '--mechanism', help='Scattering mechanism.', default = 10, action='store', type = int)   
-parser.add_argument('-p', '--plot', help='Plotting mode. 0 tunnel matrix, 1 josephson-surface, 2 josephson-current, 3 gap function, 4 fermi surface, 5 tunnel matrix times gap times fermi.', default = 10, action='store', type = int)  
+parser.add_argument('-p', '--plot', help='Plotting mode.', default = 10, action='store', type = int)  
 parser.add_argument('-k', '--fermi', help='Fermi Surface.', default = 10, action='store', type = int)  
 parser.add_argument('-b', '--band', help='Fermi Surface band.', default = 10, action='store', type = int)  
 parser.add_argument('-f', '--filename', help='Sets the filename. Only saves when given.', default = "default.png", action='store', type = str) 
-parser.add_argument('--fortran', help='Use fortran for calculation of fermi surface.', default = True, action='store', type = bool) 
+parser.add_argument('--scatter', help='Use a scatterplot instead of a surface plot.', default = False, action='store', type = bool) 
 parser.add_argument('-s', '--silent', help='Do not plot, do not save', default = False, action='store', type = bool) 
-parser.add_argument('--scatter', help='Scatter plot instead of surface. Can be quite illuminating when plotting artefacts pop up.', default = False, action='store', type = bool) 
 args	= parser.parse_args() 
 
 
@@ -39,9 +38,8 @@ plotMode	= args.plot 		#Plot Mode
 filename	= args.filename 	#Filename if we want to save
 fermi		= args.fermi 		#Type of Fermi surface
 band		= args.band	 	#Band number. There are typically 4 bands as far as I can see.
-useFortran	= args.fortran		#Use the fortran function?.
+scatter		= args.scatter	 	#Use a scatterplot instead of a surface plot..
 silent		= args.silent	 	#Silent. Don't plot, don't save.
-scatter		= args.scatter	 	#Scatter plot for fun.
  
 startTime = time.time();
 
@@ -62,8 +60,8 @@ dDelta = 10*deltaS/N
 dPhi = 2*np.pi/N
 
 #Make our arrays of parameters.
-fluxArray	= np.arange(dFlux,	fluxnum*2*np.pi+dFlux,	dFlux)
-deltaArray	= np.arange(dDelta, 	10*deltaS+dDelta, 	dDelta)
+fluxArray	= np.arange(-fluxnum*2*np.pi,	fluxnum*2*np.pi+dFlux,	dFlux)
+deltaArray	= np.arange(2*deltaS/N, 	10*deltaS+dDelta, 	dDelta)
 phiArray	= np.arange(0,			2*np.pi+dPhi, 		dPhi) 
 
 
@@ -111,50 +109,18 @@ if band >= 4:
 
 if fermi == 0:
 	Fermi = lambda kk, pp: np.max(fermiSurface);
-elif fermi == 1:   
-	if useFortran: 
-		import populate as fpopulate  
-		def Fermi (kk, pp): 
-			if len(kk.shape) == 4:
-				return fpopulate.populate.fermi_integrand(first=kk.shape[0], second=kk.shape[1], third=kk.shape[2], fourth=kk.shape[3],
-					fermi_surface=fermiSurface, angle_array=phiArray, angle=pp, radius=kk);
-			elif len(kk.shape) == 2: 
-				return fpopulate.populate.fermi_contour(first=kk.shape[0], second=kk.shape[1],
-					fermi_surface=fermiSurface, angle_array=phiArray, angle=pp, radius=kk);
-	else:
-		def Fermi( kk, pp):	
-			raise Exception("This function is being made into a fortran function");
-			# I am sorry to say that this is the best implementation I could find. My python needs some work.
-			# Anyway, if have a new mode with a new meshgrid, you'll need to add a clause.
-			fermi = pp; 
-			
-			popTime = time.time(); 
-			if len(fermi.shape) == 4: 
-				for i in range(0,fermi.shape[0]):
-					for j in range(0,fermi.shape[1]):
-						for ii in range(0,fermi.shape[2]):
-							for jj in range(0,fermi.shape[3]):
-								#fermi[i, j, ii, jj] = 1;
-								if (fermiSurface[pp[i,j, ii, jj]==phiArray][0]  > kk[i,j, ii, jj]):
-									fermi[i,j, ii, jj] = 1.  
-								else:
-									fermi[i,j, ii, jj] = 0. 
-			elif len(fermi.shape) == 2:
-				#print "Using Fermi (N+1,N+2)";
-				#Principle has been tested
-				for i in range(0, fermi.shape[0]): 
-					for j in range(0,fermi.shape[1]): 
-						#print "(%d,%d): %s %s" % (i, j, fermiSurface[pp[i,j]==phiArray][0], kk[i,j])
-						if (fermiSurface[pp[i,j]==phiArray][0]  > kk[i,j]):
-							fermi[i,j] = 1.  
-						else:
-							fermi[i,j] = 0.
-			else: 
-				raise Exception("Tuple too large.");
-			
-			print "Time taken for population is [%2.3f] s." % (time.time() - popTime);
-			#print fermi
-			return fermi;
+elif fermi == 1:  
+	import populate as fpopulate  
+	def Fermi (kk, pp):  
+		popTime = time.time(); 
+		if len(kk.shape) == 4:
+			result = fpopulate.populate.fermi_integrand(first=kk.shape[0], second=kk.shape[1], third=kk.shape[2], fourth=kk.shape[3],
+				fermi_surface=fermiSurface, angle_array=phiArray, angle=pp, radius=kk);
+		elif len(kk.shape) == 2: 
+			result =  fpopulate.populate.fermi_contour(first=kk.shape[0], second=kk.shape[1],
+				fermi_surface=fermiSurface, angle_array=phiArray, angle=pp, radius=kk);
+		print "Time taken for f.population is [%2.3f] s." % (time.time() - popTime);
+		return result
 else:
 	raise Exception("Unknown fermi surface requested.")
 
@@ -183,8 +149,8 @@ if plotMode == 0: #Plot tunnel function in k-space
 	 
 	z = tunnel(k, phi)
 	
-	plt.xlabel("$k_x$")
-	plt.ylabel("$k_y$")
+	plt.xlabel("k_x")
+	plt.ylabel("k_y")
 	title = "Tunneling Matrix";
 elif plotMode == 1:  
 	
@@ -195,8 +161,8 @@ elif plotMode == 1:
 	
 	x,y = np.meshgrid(fluxArray, deltaArray)
 	
-	plt.xlabel("$\Phi$")
-	plt.ylabel("$\Delta^0_m$")   
+	plt.xlabel("Flux")
+	plt.ylabel("Delta") 
 	title = "Current";
 elif plotMode == 2:  
 	ax.view_init(0, 90) 
@@ -208,8 +174,8 @@ elif plotMode == 2:
 	
 	x,y = np.meshgrid(fluxArray, deltaArray) 
 	
-	plt.xlabel("$\Phi$")
-	plt.ylabel("$\Delta^0_m$")   
+	plt.xlabel("Flux")
+	plt.ylabel("Delta")   
 	title = "Current_k_phi";
 elif plotMode == 3:
 	
@@ -221,8 +187,8 @@ elif plotMode == 3:
 	
 	z = Delta(1, k, phi);
 	
-	plt.xlabel("$k_x$")
-	plt.ylabel("$k_y$")
+	plt.xlabel("k_x")
+	plt.ylabel("k_y")  
 	title = "Gap function";
 elif plotMode == 4:
 	ax.view_init(30, 30) 
@@ -233,8 +199,8 @@ elif plotMode == 4:
 	
 	z =  Fermi(k, phi);
 	
-	plt.xlabel("$k_x$")
-	plt.ylabel("$k_y$")
+	plt.xlabel("k_x")
+	plt.ylabel("k_y")  
 	title = "Fermi disc";
 elif plotMode == 5:
 	
@@ -248,20 +214,16 @@ elif plotMode == 5:
 	
 	z = deltaTunnel(k, phi);
 	
-	plt.xlabel("$k_x$")
-	plt.ylabel("$k_y$")
+	plt.xlabel("k_x")
+	plt.ylabel("k_y")  
 	title = "Gap function times tunnel"; 
 else:
 	raise Exception("Unknown plot mode.");   
-
-stride = 1+int(N/150);
-
 plt.title("%s, N=%d, d=%d, m=%d, p=%d, k=%d, b=%d" % (title, N,gapfunction, mechanism, plotMode, fermi, band))
-ax.set_aspect('equal')
 if scatter:
 	ax.scatter(x,y,z);
 else:
-	ax.plot_surface(x, y, z, rstride=stride, cstride=stride, cmap=cm.summer,linewidth=0.1)  
+	ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.summer,linewidth=0, antialiased=False) 
 
 print "Elapsed time %2.3f" % (time.time() - startTime)
 if silent:
